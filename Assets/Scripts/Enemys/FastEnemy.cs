99b2d49a2aa64d3d491ec858546eccd25082eb02
menno <run_jongejan@hotmail.com>
2014-12-12 10:54:56 +0100

    art + new enemy push1.11

--- Assets/Scripts/Enemys/FastEnemy.cs
+++ Assets/Scripts/Enemys/FastEnemy.cs
@@ -6,7 +6,7 @@ public class FastEnemy : EnemyBehavior {
 	// Use this for initialization
 	protected override void Start () {
 		_speed = 1f;
-		_myGold = 50f;
+		_myGold = 10f;
 		sort = 1;
 		base.Start();
 	}
--- Assets/Scripts/Enemys/NormalEnemy.cs
+++ Assets/Scripts/Enemys/NormalEnemy.cs
@@ -6,7 +6,7 @@ public class NormalEnemy : EnemyBehavior {
 	// Use this for initialization
 	protected override void Start () {
 		_speed = 0.5f;
-		_myGold = 100f;
+		_myGold = 20f;
 		sort = 3;
 		base.Start();
 	}
